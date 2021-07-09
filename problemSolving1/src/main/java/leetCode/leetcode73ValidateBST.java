7fc6bddff5dbea529efa4542d72ef3a120d226df
ahmad <ahmad@localhost.localdomain>
2021-07-09 22:55:10 +0300

    validate BST by Recursion

--- problemSolving1/src/main/java/leetCode/leetcode73ValidateBST.java
+++ problemSolving1/src/main/java/leetCode/leetcode73ValidateBST.java
@@ -36,7 +36,7 @@ public class leetcode73ValidateBST {
 		Stack<TreeNode> stack = new Stack<TreeNode>();
 		TreeNode current = root;
 		TreeNode last = null;
-		while (! (stack.isEmpty() && current == null)) {
+		while (!(stack.isEmpty() && current == null)) {
 			if (current != null) {
 				stack.push(current);
 				current = current.left;
@@ -53,6 +53,28 @@ public class leetcode73ValidateBST {
 	}
 
 	/**
+	 *  in order BST
+	 * @param root
+	*/
+	//private boolean isBST2 = true;
+	private TreeNode prev = new TreeNode(Integer.MIN_VALUE);;
+
+	public boolean isBSTbyRecursion(TreeNode root) {
+		return dfsInorder(root, true);
+	}
+
+	public boolean dfsInorder(TreeNode root, boolean isBST2) {
+		if (root == null)
+			return isBST2;
+		isBST2 = dfsInorder(root.left,isBST2);
+		if (prev.val >= root.val)
+			return false;
+		prev = root;
+		isBST2 = dfsInorder(root.right,isBST2);
+		return isBST2;
+	}
+
+	/**
 	 * 
 	 * @param args
 	*/
@@ -63,7 +85,7 @@ public class leetcode73ValidateBST {
 		TreeNode node5 = new TreeNode(5);
 		TreeNode node1 = new TreeNode(1);
 		TreeNode node2 = new TreeNode(2, node1, node3);
-		TreeNode node6 = new TreeNode(6, node5, node7);
+		TreeNode node6 = new TreeNode(6, node7, node5);
 		TreeNode node4 = new TreeNode(4, node2, node6);
 		leetcode73ValidateBST bst = new leetcode73ValidateBST();
 
@@ -71,14 +93,15 @@ public class leetcode73ValidateBST {
 		System.out.println(bst.isBST(node4));
 
 		// demorgan's theorem
-		System.out.println("===== demorgan's theorem =====");
-		System.out.println(!(false || true));
-		System.out.println((true && false));
-
+		//		System.out.println("===== demorgan's theorem =====");
+		//		System.out.println(!(false || true));
+		//		System.out.println((true && false));
 
 		// print hashcode as class@hashCodeByHex convert it to class@HashCodeByDecimal by Integer.parse(int,16)
-		System.out.println("===== print hashcode class@HashCodeByHex =====");
-		System.out.println(" node4 = " + node4); // print hashcode as class@hashCodeByHex convert it to class@HashCodeByDecimal by Integer.parse(int,16)
+		//		System.out.println("===== print hashcode class@HashCodeByHex =====");
+		//		System.out.println(" node4 = " + node4); // print hashcode as class@hashCodeByHex convert it to class@HashCodeByDecimal by Integer.parse(int,16)
 
+		System.out.println("===== print  isBST by Recursion=====");
+		System.out.println("result = " + bst.isBSTbyRecursion(node4));
 	}
 }
