10cf45a27c675bbe6e8da0aeb2a26761b9112a1a
ahmad <ahmad@localhost.localdomain>
2021-08-03 10:26:32 +0300

    add recursive combinations leetcode 50

--- problemSolving1/src/main/java/leetCode/Leetcode50Combinations.java
+++ problemSolving1/src/main/java/leetCode/Leetcode50Combinations.java
@@ -58,29 +58,30 @@ public class Leetcode50Combinations {
 		Deque<TreeComb> stack = new ArrayDeque<>(); // faster than stack
 		stack.push(root);
 		while (!stack.isEmpty()) {
-			TreeComb node = stack.pop(); // DFS by pre-order 
+			TreeComb node = stack.pop(); // DFS by pre-order
+			System.out.println(node.toString());
 			while ((node.getListOfInt().size() < k) && (node.getFromIndex() <= nums.length - 1)) {
 				TreeComb child = new TreeComb(node.getListOfInt(), node.getFromIndex());
 				child.getListOfInt().add(nums[node.getFromIndex()]);
 				//				if (child.getSumOfListOfInt() <= target) { // works correct  without this condition
 				node.getListOfTree().add(child);
-				stack.push(child);
+				//stack.push(child);
 				//				}
 				node.setFromIndex(node.getFromIndex() + 1);
+			}
+			if (!node.getListOfTree().isEmpty()) { // add children to stack reversed without change the children sort
+				node.getListOfTree().stream() // Stream
+						.collect(Collectors.toCollection(LinkedList::new)) // LinkedList
+						.descendingIterator() // Iterator
+						.forEachRemaining(stack::push);
+
+				// for (TreeComb element : node.getListOfTree()) { // above same as below but it is changet the children sort
+				// 	stack.push(element);
+				// }
 			}
 			if (node.getListOfInt().size() == k && node.passListNoDuplicateElements())
 				result.add(node.getListOfInt());
 
-			// if (!node.getListOfTree().isEmpty()) { // add children to stack reversed without change the children sort
-			// 	node.getListOfTree().stream() // Stream
-			// 			.collect(Collectors.toCollection(LinkedList::new)) // LinkedList
-			// 			.descendingIterator() // Iterator
-			// 			.forEachRemaining(stack::push);
-
-			// for (TreeComb element : node.getListOfTree()) { // above same as below but it is changet the children sort
-			// 	stack.push(element);
-			// }
-			//			}
 			// if (node.getSumOfListOfInt() == target) {
 			// 	result.add(node.getListOfInt());
 			// 	System.out.println("node == target " + node.getListOfInt().toString());
@@ -93,7 +94,43 @@ public class Leetcode50Combinations {
 		} // end of if
 			//Collections.reverse(result); // result = [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
 		return result;
+	}
 
+	/**
+	* 
+	* 
+	*/
+	List<List<Integer>> results;
+
+	public List<List<Integer>> combine(int n, int k) {
+		results = new ArrayList<List<Integer>>();
+
+		recurse(new ArrayList<Integer>(), 1, n, k);
+		return results;
+	}
+
+	/**
+	* 
+	* @param intList
+	* @param curr
+	* @param n
+	* @param k
+	*/
+	public void recurse(List<Integer> intList, int curr, int n, int k) {
+		if (k == intList.size()) {
+			results.add(new ArrayList<>(intList));
+			return;
+		}
+
+		// 1 2 3 4 5 6
+		for (int i = curr; i < n + 1; i++) {
+			if (k - intList.size() <= n - i + 1) {
+				intList.add(i);
+				System.out.println(intList);
+				recurse(intList, i + 1, n, k);
+				intList.remove(intList.size() - 1);
+			}
+		}
 	}
 
 	/**
@@ -102,13 +139,18 @@ public class Leetcode50Combinations {
 	*/
 	public static void main(String[] args) {
 		Leetcode50Combinations comb = new Leetcode50Combinations();
-		int n = 4;
-		int k = 2;
+		int n = 5;
+		int k = 4;
+
 		List<List<Integer>> result = comb.combinations(n, k);
 		System.out.println("========== print result  ================ ");
-		System.out.println("result = "+result.toString());
+		System.out.println("result = " + result.toString());
 
 		System.out.println("========== print full tree ================ ");
 		comb.printFullCombinationTree(comb.root);
+		System.out.println("the End");
+
+		System.out.println("========== print results by recursion ================ ");
+		System.out.println("results" + comb.combine(n, k));
 	}
 }
